976fca25ec3d93dbf4187f2d186ea5a9143d1b4a
Lutforurc <lutforurc@gmail.com>
2025-10-16 02:20:53 +0600

    udpate

--- src/components/modules/transactions/bankreceived/BankReceived.tsx
+++ src/components/modules/transactions/bankreceived/BankReceived.tsx
@@ -127,6 +127,8 @@ const BankReceived = () => {
   };
 
 
+  
+
   const mapReceivedData = (res: any): ReceivedItem => {
     const data = res.data.data;
     const details = data.acc_transaction_master[0].acc_transaction_details;
