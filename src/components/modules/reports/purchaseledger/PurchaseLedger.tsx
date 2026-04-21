85f215b5c3be72827eb81075c04aa8c29f4c764f
Lutfor Rahman <lutforurc@gmail.com>
2026-04-22 00:50:55 +0600

    Update

--- src/components/modules/reports/purchaseledger/PurchaseLedger.tsx
+++ src/components/modules/reports/purchaseledger/PurchaseLedger.tsx
@@ -241,6 +241,9 @@ const PurchaseLedger = (user: any) => {
           <div className="text-left">
             <span className='block'>{ formatTransportationNumber(row?.purchase_master?.vehicle_no)}</span>
              <span className='text-green-500 dark:text-yellow-300 '>{ row?.purchase_master?.purchase_order?.order_number}</span>
+             { row?.purchase_master?.purchase_order?.delivery_location && (
+               <span className='text-green-500 dark:text-yellow-300 '>{ row?.purchase_master?.purchase_order?.delivery_location}</span>
+             )}
           </div>
         );
       },
