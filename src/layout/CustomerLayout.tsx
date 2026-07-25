99514bbe8fb9f11d0b264e4cc27bf3daba341462
Lutfor Rahman <lutforurc@gmail.com>
2026-07-25 13:22:31 +0600

    Update

--- src/layout/CustomerLayout.tsx
+++ src/layout/CustomerLayout.tsx
@@ -7,6 +7,7 @@ import Loader from "../common/Loader";
 // import Header from "../components/Header/index";
 // import Sidebar from "../components/Sidebar/index";
 
+
 const CustomerLayout: React.FC = () => {
   const location = useLocation();
   const dispatch = useDispatch();
