1e7c498e5080d81de37f78d7b6ae7c0f15196b9f
Lutfor Rahman <lutforurc@gmail.com>
2026-08-15 01:05:57 +0600

    Last Movement

--- src/components/modules/invoices/purchase/ConstructionBusinessPurchase.tsx
+++ src/components/modules/invoices/purchase/ConstructionBusinessPurchase.tsx
@@ -691,7 +691,7 @@ const ConstructionBusinessPurchase = () => {
   return (
     <>
       <div className="mb-2 flex flex-wrap items-center justify-center gap-2">
-        <HelmetTitle title="Construction Purchase Invoice" screen="purchase.construction" />
+        <HelmetTitle title="Purchase Invoice" screen="purchase.construction" />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-8">
         {purchase.isLoading ? <Loader /> : null}
